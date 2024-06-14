990a8b697babdbb8ba3ca2e79c9159ce2eee77bc
ravimann <ravimann@CSGBTPLT410.creative.local>
2024-06-14 17:26:29 +0530

    docker integration added

--- JavaTestNgParellel/src/main/java/testbase/BrowserFactory.java
+++ JavaTestNgParellel/src/main/java/testbase/BrowserFactory.java
@@ -1,19 +1,27 @@
 package testbase;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BrowserFactory {
 	
-	public WebDriver createBrowserInstance(String browser) {
+	public WebDriver createBrowserInstance(String browser) throws MalformedURLException {
 		WebDriver driver = null;
 		
 		if(browser.equals("chrome")) {
-			ChromeOptions opt = new ChromeOptions();
-			driver = new ChromeDriver(opt);
+			ChromeOptions options = new ChromeOptions();
+			//driver = new ChromeDriver(opt);
+			
+			options.addArguments("--remote-allow-origins=*");
+	        driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+	        driver.manage().window().maximize();
 		}
 		else if(browser.equals("firefox")) {
 			driver = new FirefoxDriver();
