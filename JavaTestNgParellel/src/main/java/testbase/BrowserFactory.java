958dc18b03ea2ac841c771d7d8efd4c4eaa0ecff
ravimann <ravimann@CSGBTPLT410.creative.local>
2024-06-15 07:35:10 +0530

    Code Updated

--- JavaTestNgParellel/src/main/java/testbase/BrowserFactory.java
+++ JavaTestNgParellel/src/main/java/testbase/BrowserFactory.java
@@ -1,27 +1,19 @@
 package testbase;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BrowserFactory {
 	
-	public WebDriver createBrowserInstance(String browser) throws MalformedURLException {
+	public WebDriver createBrowserInstance(String browser) {
 		WebDriver driver = null;
 		
 		if(browser.equals("chrome")) {
-			ChromeOptions options = new ChromeOptions();
-			//driver = new ChromeDriver(opt);
-			
-			options.addArguments("--remote-allow-origins=*");
-	        driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
-	        driver.manage().window().maximize();
+			ChromeOptions opt = new ChromeOptions();
+			driver = new ChromeDriver(opt);
 		}
 		else if(browser.equals("firefox")) {
 			driver = new FirefoxDriver();
